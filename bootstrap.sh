a8bd6be11223ea7f416766c42cbd4829e020395f
Sarah Anderson <seanderson07@gmail.com>
2015-03-03 15:05:32 -0500

    Updates ruby version from 2.1.2 to 2.2.1. Updates Rails version 4.0.5 to 4.2.0. Includes additional apt-get for libffi-dev needed for updated version of Ruby. Changes localhost port from 3001 to 3000.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,14 @@
 RAILS_APP=railsapp
 
 ## Aditional system tools
+sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y nodejs
 sudo apt-get install -y imagemagick
 sudo apt-get install -y libsqlite3-dev
 sudo apt-get install -y sqlite3
 sudo apt-get install -y build-essential
+sudo apt-get install -y libffi-dev
 
 ## Install rbenv and rbenv-build to manage ruby environment
 su - vagrant -c "git clone https://github.com/sstephenson/rbenv.git ~/.rbenv"
@@ -17,13 +19,13 @@ su - vagrant -c "echo 'export PATH=\"~/.rbenv/bin:$PATH\"' >> ~/.bash_profile"
 su - vagrant -c "echo 'eval \"\$(rbenv init -)\"' >> ~/.bash_profile"
 su - vagrant -c "git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
 
-## Install Ruby 2.1.2
-su - vagrant -c "rbenv install 2.1.2"
-su - vagrant -c "rbenv global 2.1.2"
+## Install Ruby 2.2.1
+su - vagrant -c "rbenv install 2.2.1"
+su - vagrant -c "rbenv global 2.2.1"
 su - vagrant -c "rbenv rehash"
 
-## Install Rails 4.0.5
-su - vagrant -c "gem install rails"
+## Install Rails 4.2.0
+su - vagrant -c "gem install rails -v 4.2.0"
 su - vagrant -c "rbenv rehash"
 
 ## Setup new app
